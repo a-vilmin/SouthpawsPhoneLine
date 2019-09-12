2c0521cc7bfddecd30e4d9f5e3b1e84c2d8a468e
Adam Vilmin <avilmin@thinq.com>
2019-09-12 09:23:54 -0400

    added texting

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from twilio.twiml.voice_response import VoiceResponse
+from twilio.twiml.voice_response import VoiceResponse, MessagingResponse
 
 app = Flask(__name__)
 
@@ -13,6 +13,18 @@ def voice():
     response.hangup()
 
     return str(response)
+
+
+@app.route("/sms", methods=['GET', 'POST'])
+def sms_reply():
+    """Respond to incoming calls with a simple text message."""
+    # Start our TwiML response
+    resp = MessagingResponse()
+
+    # Add a message
+    resp.message("Message recieved, thanks for contributing to the Darren Rovell fan data survey. Your input is valuable for knowing what the official soft drink of 9/11 will be.")
+
+    return str(resp)
 
 
 if __name__ == "__main__":
